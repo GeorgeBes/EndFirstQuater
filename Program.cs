7fd57827696b1b0d88115836c82e670f438af298
George <bortnikow1991@mail.ru>
2022-11-27 13:27:23 +0300

    Решил задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длинна которых
+// меньше либо равна 3 символа.
+
+string[] arrayFirst = new string[7] { "hello", "23", "wor", ":-)", "1223", "1567", "com" };
+string[] arraySecond = new string[arrayFirst.Length];
+void MethodNewArray(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[count] = arr1[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+MethodNewArray(arrayFirst, arraySecond);
+PrintArray(arraySecond);
